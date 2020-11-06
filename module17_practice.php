b9351fb2687d1aecc6549fc6f7803c655fd408d1
ILYA <iliad1@yandex.ru>
2020-11-06 21:28:51 +0700

    renamed

---
+++ module17_practice.php
@@ -0,0 +1,75 @@
+<?php
+$example_persons_array = [
+	[
+		'fullname' => 'Иванов Иван Иванович',
+		'job' => 'tester',
+	],
+	[
+		'fullname' => 'Степанова Наталья Степановна',
+		'job' => 'frontend-developer',
+	],
+	[
+		'fullname' => 'Пащенко Владимир Александрович',
+		'job' => 'analyst',
+	],
+	[
+		'fullname' => 'Громов Александр Иванович',
+		'job' => 'fullstack-developer',
+	],
+	[
+		'fullname' => 'Славин Семён Сергеевич',
+		'job' => 'analyst',
+	],
+	[
+		'fullname' => 'Цой Владимир Антонович',
+		'job' => 'frontend-developer',
+	],
+	[
+		'fullname' => 'Быстрая Юлия Сергеевна',
+		'job' => 'PR-manager',
+	],
+	[
+		'fullname' => 'Шматко Антонина Сергеевна',
+		'job' => 'HR-manager',
+	],
+	[
+		'fullname' => 'аль-Хорезми Мухаммад ибн-Муса',
+		'job' => 'analyst',
+	],
+	[
+		'fullname' => 'Бардо Жаклин Фёдоровна',
+		'job' => 'android-developer',
+	],
+	[
+		'fullname' => 'Шварцнегер Арнольд Густавович',
+		'job' => 'babysitter',
+	],
+];
+
+//1.Разбиение ФИО
+getPartsFromFullname
+
+
+
+//2.Объединение ФИО
+getPartsFromFullname
+
+
+
+//3.Сокращение ФИО
+getShortName
+
+
+
+//4.Определение пола по ФИО
+getGenderFromName
+
+
+
+//5.Определение возрастно-полового состава
+getGenderDescription 
+
+
+
+//6.Идеальный подбор пары
+getPerfectPartner 
