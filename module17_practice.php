02f7fb849ec78dfe2b68253452b310f3ffa1aaa5
ILYA <iliad1@yandex.ru>
2020-11-07 20:16:05 +0700

    add tasks 1-4 finished

--- module17_practice.php
+++ module17_practice.php
@@ -46,54 +46,71 @@ $example_persons_array = [
 	],
 ];
 
-$n = 0;
+$n = 1;
 $fullname = $example_persons_array[$n]['fullname'];
-$surname = $example_persons_array[$n]['fullname'];
-$name = $example_persons_array[$n]['fullname'];
 
+$partsFromFullname = getPartsFromFullname($fullname);
+$surname = $partsFromFullname['surname'];
+$name = $partsFromFullname['name'];
+$patronymic = $partsFromFullname['patronomyc'];
 
+echo ($fullname);
+echo ('</br>');
 
 //1.Разбиение ФИО
 function getPartsFromFullname($fullname)
 {
-	$part1 = ['surname', 'name', 'patronomyc'];
-	$part2 = explode(' ', $fullname);
-	return array_combine($part1, $part2);
+	return array_combine(['surname', 'name', 'patronomyc'], explode(' ', $fullname));
 };
 
 print_r(getPartsFromFullname($fullname));
-
-/*
-	if ($n == 0) return 0; 
-	if (($n == 1) || ($n == 2)) { 
-	 return 1; 
-	} else { 
-	 return fibo($n - 1) + fibo($n -2); 
-	};
-  };
-
+echo ('</br>');
 
 //2.Объединение ФИО
-/*function getFullnameFromParts($surname, $name, $patronymic) { 
-	if ($n == 0) return 0; 
-	if (($n == 1) || ($n == 2)) { 
-	 return 1; 
-	} else { 
-	 return fibo($n - 1) + fibo($n -2); 
-	};
-  };*/
+function getFullnameFromParts($surname, $name, $patronymic)
+{
+	return "$surname $name $patronymic";
+};
 
-
+echo getFullnameFromParts($surname, $name, $patronymic);
+echo ('</br>');
 
 //3.Сокращение ФИО
-//getShortName
+function getShortName($fullname)
+{
+	$partsFromFullname = getPartsFromFullname($fullname);
+	$name = $partsFromFullname['name'];
+	$surname = $partsFromFullname['surname'];
+	$shortSurname = mb_strimwidth("$surname", 0, 2, ".");
+	return "$name $shortSurname";
+};
 
-
+echo getShortName($fullname);
+echo ('</br>');
 
 //4.Определение пола по ФИО
-//getGenderFromName
+function getGenderFromName($fullname)
+{
+	$partsFromFullname = getPartsFromFullname($fullname);
+	$name = $partsFromFullname['name'];
+	$surname = $partsFromFullname['surname'];
+	$patronymic = $partsFromFullname['patronomyc'];
+	$genderMail = 0;
+	$genderFemail = 0;
 
+	if (mb_substr($patronymic, -2, 2) == 'ич') $genderMail++;
+	if ((mb_substr($name, -1, 1) == 'й') || (mb_substr($name, -1, 1) == 'н')) $genderMail++;
+	if (mb_substr($surname, -1, 1) == 'в') $genderMail++;
+	if (mb_substr($patronymic, -3, 3) == 'вна') $genderFemail++;
+	if (mb_substr($name, -1, 1) == 'а') $genderFemail++;
+	if (mb_substr($surname, -2, 2) == 'ва') $genderFemail++;
 
+	return ($genderMail <=> $genderFemail);
+};
+
+echo getGenderFromName($fullname);
+
+//print_r(mb_substr($patronymic, -3, 3) == 'вна');
 
 //5.Определение возрастно-полового состава
 //getGenderDescription 
